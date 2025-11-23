a87fc8b813200f3f7d1047ba54a39d57bf5805c8
Deepak_Mane <manedeepak1902@gmail.com>
2025-11-24 00:15:25 +0530

    alll data configure

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,9 @@
-export const asyncHandler =(reqHandler)=>{
-     (req,res,next)=>{
-          Promise.resolve(reqHandler()).catch((err)=>{next(err)})
-     }
-}
+// export const asyncHandler =(reqHandler)=>{
+//      return (req,res,next)=>{
+//           Promise.resolve(reqHandler(res)).catch((err)=>{next(err)})
+//      }
+// }
+
+export const asyncHandler = (reqHandler) => (req, res, next) => {
+  Promise.resolve(reqHandler(req, res, next)).catch(next);
+};
